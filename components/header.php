e5739e658a250a2b6abb5094beb90de85735c576
Edo <hello@edoardo-hb.it>
2024-01-25 16:52:52 +0100

    feat: bonus 1

---
+++ components/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <title>Lista Hotel</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="mt-5 mb-4">Lista Hotel</h1>
--- index.php
+++ index.php
@@ -39,42 +39,27 @@ $hotels = [
 ];
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <title>Lista Hotel</title>
-</head>
-<body>
-  <div class="container">
-    <h1 class="mt-5 mb-4">Lista Hotel</h1>
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Descrizione</th>
-          <th>Parcheggio</th>
-          <th>Voto</th>
-          <th>Distanza dal centro</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($hotels as $hotel) { ?>
-          <tr>
-            <td><?php echo $hotel['name'] ?></td>
-            <td><?php echo $hotel['description'] ?></td>
-            <td><?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile' ?></td>
-            <td><?php echo $hotel['vote'] ?></td>
-            <td><?php echo $hotel['distance_to_center'] ?> km</td>
-          </tr>
-        <?php } ?>
-      </tbody>
-    </table>
-  </div>
-
-</body>
-
-</html>
+<?php include './components/header.php'; ?>
+<table class="table">
+  <thead>
+    <tr>
+      <th>Nome</th>
+      <th>Descrizione</th>
+      <th>Parcheggio</th>
+      <th>Voto</th>
+      <th>Distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($hotels as $hotel) { ?>
+      <tr>
+        <td><?php echo $hotel['name'] ?></td>
+        <td><?php echo $hotel['description'] ?></td>
+        <td><?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile' ?></td>
+        <td><?php echo $hotel['vote'] ?></td>
+        <td><?php echo $hotel['distance_to_center'] ?> km</td>
+      </tr>
+    <?php } ?>
+  </tbody>
+</table>
+<?php include './components/footer.php'; ?>
